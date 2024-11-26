678e582a77940afbc81802dba546c7c62b12ae6d
Pong <pongpong34756@gmail.com>
2024-11-26 16:59:33 +0700

    finish mini projectA

--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -1,15 +1,70 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUserPlus,
+  faRocket,
+  faHeart,
+  faArrowUpRightDots,
+} from "@fortawesome/free-solid-svg-icons";
+
 function Skill() {
   return (
-    <section className="min-h-[400px] p-10 flex flex-col items-center">
-      <div className="flex flex-col gap-10">
+    <section className="min-h-[400px] p-10 flex flex-col bg-gray-200">
+      <div className="flex flex-col gap-10 justify-center items-center">
         <div className="underline font-semibold text-[30px] md:text-[40px] lg:text-[50px]">
           Experiences & Skills
         </div>
-        <div className="flex flex-col gap-5 lg:flex lg:flex-row">
-          <div className="bg-orange-400">Team Work</div>
-          <div className="bg-red-400">Skills</div>
-          <div className="bg-purple-400">Mentality</div>
-          <div className="bg-lime-400">Growth</div>
+        <div className="flex flex-col items-center gap-10 lg:flex lg:flex-row">
+          <div className=" bg-orange-400 hover:bg-orange-600 duration-500 flex flex-col justify-center items-center gap-5 p-5 rounded-[20px] shadow-xl ">
+            <div className="text-[30px] md:text-[40px] lg:text-[50px]">
+              TeamWork
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faUserPlus} className="min-h-[50px]" />
+            </div>
+            <div className="text-[15px] md:text-[20px] lg:text-[25px]">
+              Working with us will enhance your teamwork skills and improve
+              communication efficiency.
+            </div>
+          </div>
+          <div className=" bg-red-400 hover:bg-red-600 duration-500 flex flex-col justify-center items-center gap-5 p-5 rounded-[20px] shadow-xl ">
+            <div className="text-[30px] md:text-[40px] lg:text-[50px]">
+              Skills
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faRocket} className="min-h-[50px]" />
+            </div>
+            <div className="text-[15px] md:text-[20px] lg:text-[25px]">
+              Working with us will enhance your teamwork skills and improve
+              communication efficiency.
+            </div>
+          </div>
+          <div className=" bg-purple-400 hover:bg-purple-600 duration-500 flex flex-col justify-center items-center gap-5 p-5 rounded-[20px] shadow-xl ">
+            <div className="text-[30px] md:text-[40px] lg:text-[50px]">
+              Mentality
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faHeart} className="min-h-[50px]" />
+            </div>
+            <div className="text-[15px] md:text-[20px] lg:text-[25px]">
+              Working with us will enhance your teamwork skills and improve
+              communication efficiency.
+            </div>
+          </div>
+          <div className=" bg-lime-400 hover:bg-lime-600 duration-500 flex flex-col justify-center items-center gap-5 p-5 rounded-[20px] shadow-xl ">
+            <div className="text-[30px] md:text-[40px] lg:text-[50px]">
+              Growth
+            </div>
+            <div>
+              <FontAwesomeIcon
+                icon={faArrowUpRightDots}
+                className="min-h-[50px]"
+              />
+            </div>
+            <div className="text-[15px] md:text-[20px] lg:text-[25px]">
+              Working with us will enhance your teamwork skills and improve
+              communication efficiency.
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -2,6 +2,7 @@ import Header from "../components/header";
 import Banner from "../components/banner";
 import Intro from "../components/intro";
 import Skill from "../components/skill";
+import Contact from "../components/contact";
 
 function Landingpage() {
   return (
@@ -10,6 +11,7 @@ function Landingpage() {
       <Intro />
       <Banner />
       <Skill />
+      <Contact />
     </div>
   );
 }
